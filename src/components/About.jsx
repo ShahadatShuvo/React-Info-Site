051971c628d230c471de0d7fd96ab33afe5f789f
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-03-20 13:29:59 +0600

    code updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 
 import bgImg from "../images/reactjs-icon 2.svg";
 
 function About(props) {
   return (
     <section className={props.darkMode ? "about" : "about bg-light text-dark"}>
-    <div className="container-fluid content">
-      <img src={bgImg} className="bgImg" alt="" />
-      <div className="container pt-5">
-        <div className="row">
-          <div className="col">
-            <h1 className="fw-bold">Fun facts about React</h1>
-            <ul className="lh-lg fs-4">
-              <li>Was first released in 2013</li>
-              <li>Was originally created by Jordan Walke</li>
-              <li>Has well over 100K stars on GitHub</li>
-              <li>Is maintained by Facebook</li>
-              <li>
-                Powers thousands of enterprise apps, including mobile apps
-              </li>
-            </ul>
+      <div className="container-fluid content">
+        {props.darkMode && <img src={bgImg} className="bgImg" alt="" />}
+        <div className="container pt-5">
+          <div className="row">
+            <div className="col">
+              <h1 className="fw-bold">Fun facts about React</h1>
+              <ul className="lh-lg fs-4">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>
+                  Powers thousands of enterprise apps, including mobile apps
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default About
+export default About;
